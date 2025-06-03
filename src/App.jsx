4f374092e2a675b655a5606244bff5ef1e4103c2
Aya-Kukua <ayakukua001@gmail.com>
2025-06-03 10:38:35 +0000

    rename of file

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Navbar from "./components/Navbar"
 export default function App() {
   return (
     <>
-    <Navbar/>
-<h1 className="text-4xl text-center font-bold italic">Welcome to Event Hive</h1>
-<Footer/>
+      <Navbar />
+      <h1 className="text-4xl text-center font-bold italic">Welcome to Event Hive</h1>
+      <Footer />
     </>
   )
 }
